56cf6ef616822f141d319bfa00c79c335f0d5713
michelebelfiore <m.belfiore@eurolink.it>
2019-09-09 09:03:03 +0200

    jtest added

--- BackEnd/azienda/src/test/java/com/almaviva/azienda/AziendaApplicationTests.java
+++ BackEnd/azienda/src/test/java/com/almaviva/azienda/AziendaApplicationTests.java
@@ -1,21 +1,54 @@
 package com.almaviva.azienda;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import com.azienda.controller.DipendenteContoller;
+import com.azienda.AziendaApplication;
+import com.google.common.net.MediaType;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = DipendenteContoller.class)
+@SpringBootTest(classes = AziendaApplication.class)
+@ContextConfiguration
 public class AziendaApplicationTests {
 
-	
-    @Test
-    public void contextLoads() {
-    }
 
+
+    @Autowired
+	private WebApplicationContext webAppContext;
+	
+	private MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.webAppContextSetup(webAppContext).build();
+	}
+	
+	@Test
+	public void  testDip() throws Exception {
+		mockMvc.perform(get("/dipendenti")).andExpect(status().isNoContent());
+	}
+	
+	@Test
+	public void testEmployee() throws Exception {
+		mockMvc.perform(post("/addDip")).andExpect(status().isOk());
+
+	}
+
+	
 }
   
